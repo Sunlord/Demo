da3504bcd026eee81cbe66f3ad3e774229a9b252
Sunlord <gj2011sd2011@163.com>
2018-04-05 15:44:36 +0800

    add test

--- Helloworld.java
+++ Helloworld.java
@@ -3,5 +3,6 @@ package com.imooc;
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Helloworld!!");
+		System.out.println("Welcome to use github!");
 	}
 }
