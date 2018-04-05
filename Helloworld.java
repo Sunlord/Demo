f406600bab8bcf6f9faf34fa6deb17d78130c39b
Sunlord <gj2011sd2011@163.com>
2018-04-05 16:47:50 +0800

    Revert "add test"
    This reverts commit da3504bcd026eee81cbe66f3ad3e774229a9b252.

--- Helloworld.java
+++ Helloworld.java
@@ -3,6 +3,5 @@ package com.imooc;
 public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Helloworld!!");
-		System.out.println("Welcome to use github!");
 	}
 }
