27dd644e3d75ec2b2710da49f91c82c44c766eae
Sunlord <gj2011sd2011@163.com>
2018-04-06 14:23:39 +0800

    clean out things

--- Helloworld.java
+++
@@ -1,7 +0,0 @@
-package com.imooc;
-
-public class Helloworld {
-	public static void main(String[] args) {
-		System.out.println("Helloworld!!");
-	}
-}
--- idea.java
+++
@@ -1,7 +0,0 @@
-package com.imooc;
-
-public class idea {
-	public static void main(String[] args) {
-		System.out.println("I have a new Idea!!");
-	}
-}
